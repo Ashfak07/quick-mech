59f10789e0a4992735c724d6fe03916ee4d27e6c
Ashfak07 <ashfakashraf54@gmail.com>
2023-11-07 10:43:39 +0530

    folder created

---
+++ lib/view/intro_screen/intro_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class INtroScreen extends StatelessWidget {
+  const INtroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
