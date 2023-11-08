694a81a620fca1f1151a78ee470dddac58baf9ba
Cideep   krishnan <cideep671@gmail.com>
2023-11-08 12:02:02 +0530

    first_commit

--- lib/view/splash_screen/splash_screen.dart
+++ lib/view/splash_screen/splash_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3))
+        .then((value) => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SplashScreen(),
+            )));
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 307,
+        ),
+      ),
+    );
   }
 }
