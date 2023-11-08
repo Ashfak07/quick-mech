e0879f434c80bcac3344f92449ffdd0137853c10
AbhishekBalachandran <abhishekbambady2014@gmail.com>
2023-11-08 12:53:26 +0530

    dark-mode-added

---
+++ lib/controller/project-controller/project-controller.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ProjectController with ChangeNotifier {
+  bool isDark  = false;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:quickmech/view/login_screen/login_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:quickmech/controller/project-controller/project-controller.dart';
+import 'package:quickmech/view/bottom_navigation_bar/bottom_navigation_bar.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(List<String> args) {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const LoginScreen());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProjectController(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: Provider.of<ProjectController>(context).isDark
+            ? ThemeData.dark()
+            : ThemeData.light(),
+        home: Scaffold(),
+      ),
+    );
   }
 }
