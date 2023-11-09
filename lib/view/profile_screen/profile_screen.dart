782b4e215fb167af894489ac376c2009cf0a954e
Vyshnav k m <vyshnavkma06@gmail.com>
2023-11-09 10:11:12 +0530

    profile screen update

--- lib/view/profile_screen/profile_screen.dart
+++ lib/view/profile_screen/profile_screen.dart
@@ -13,67 +13,89 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       body: ListView(
         children: [
+          Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Hey, Flutter',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                      ],
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Divider(
+                  thickness: 5,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 40,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 4),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                          child: Text(
+                        'Payment',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                    Container(
+                      height: 40,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 4),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                          child: Text(
+                        'Activity',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                    Container(
+                      height: 40,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 4),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                          child: Text(
+                        'Help',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 0,
+                )
+              ],
+            ),
+          ),
           SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                'Hey, Flutter',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              CircleAvatar(
-                radius: 40,
-              )
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                height: 40,
-                width: 100,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 4),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Center(
-                    child: Text(
-                  'Payment',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-              ),
-              Container(
-                height: 40,
-                width: 100,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 4),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Center(
-                    child: Text(
-                  'Activity',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-              ),
-              Container(
-                height: 40,
-                width: 100,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 4),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Center(
-                    child: Text(
-                  'Help',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 100,
+            height: 50,
           ),
           Column(
             children: [
