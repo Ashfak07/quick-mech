9ba7a6d9e035c606d8983228b01aad52dcdceef8
Cideep   krishnan <cideep671@gmail.com>
2023-11-10 00:11:59 +0530

    validation in profile edit screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quickmech/view/Edit_profile/edit_profile.dart';
 import 'package:quickmech/view/bottom_navigation_bar/bottom_navigation_bar.dart';
-import 'package:quickmech/view/home_screen/home_screen.dart';
-import 'package:quickmech/view/login_screen/login_screen.dart';
 import 'package:quickmech/view/splash_screen/splash_screen.dart';
 
 void main(List<String> args) {
@@ -14,6 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, home: const BottomNavBar());
+        debugShowCheckedModeBanner: false, home: const SplashScreen());
   }
 }
