6850752149e119d17a9297ac8ffc55e73bb3555e
Vyshnav k m <vyshnavkma06@gmail.com>
2023-11-09 18:25:51 +0530

    screen changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quickmech/view/profile_screen/profile_screen.dart';
 import 'package:quickmech/view/splash_screen/splash_screen.dart';
 
 void main(List<String> args) {
@@ -11,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const ProfileScreen());
+    return MaterialApp(home: const SplashScreen());
   }
 }
