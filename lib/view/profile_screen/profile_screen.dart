acbe24152175a1f643cfd46e7d934fc71ef1e87e
Vyshnav k m <vyshnavkma06@gmail.com>
2023-11-10 10:46:52 +0530

    size changed

--- lib/view/profile_screen/profile_screen.dart
+++ lib/view/profile_screen/profile_screen.dart
@@ -130,7 +130,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     onPressed: () {},
                     icon: Icon(
                       Icons.arrow_circle_right_rounded,
-                      size: 40,
+                      size: 41,
                     ),
                   )),
             ],
