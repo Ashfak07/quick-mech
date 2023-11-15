29fb4cf27accba5f3ebf7c8b110eb066ca34ff44
Vyshnav k m <vyshnavkma06@gmail.com>
2023-11-15 19:15:08 +0530

    Terms And Conditions changed

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,7 @@ import 'package:quickmech/utils/color_constants.dart';
 import 'package:provider/provider.dart';
 import 'package:quickmech/controller/booking_page_controller/booking_page_controller.dart';
 import 'package:quickmech/controller/mechanic_profile_controller/mechanic_profile_controller.dart';
-import 'package:quickmech/view/bottom_navigation_bar/bottom_navigation_bar.dart';
 import 'package:quickmech/utils/database/database_for%20_favourite.dart';
-import 'package:quickmech/view/categorywise_worker_list/categorywise_workers_list.dart';
-
-import 'package:quickmech/view/intro_screen/intro_screen.dart';
-import 'package:quickmech/view/offer_screen/offers.dart';
 import 'package:quickmech/view/splash_screen/splash_screen.dart';
 
 void main(List<String> args) {
@@ -45,9 +40,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-
         home: SplashScreen(),
-
         theme: ThemeData(
             colorScheme:
                 ColorScheme.fromSeed(seedColor: ColorConstants.bannerColor),
