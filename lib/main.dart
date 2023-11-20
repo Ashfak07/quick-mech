5d129ab56ef924ca9fe406ae99c32f45c20d8509
Vyshnav k m <vyshnavkma06@gmail.com>
2023-11-20 11:10:54 +0530

    mechanic module added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:quickmech/controller/booking_page_controller/booking_page_contro
 import 'package:quickmech/controller/mechanic_profile_controller/mechanic_profile_controller.dart';
 import 'package:quickmech/utils/database/database_for%20_favourite.dart';
 import 'package:quickmech/view/bottom_navigation_bar/bottom_navigation_bar.dart';
-import 'package:quickmech/view/intro_screen/intro_screen.dart';
+
 
 void main(List<String> args) {
   runApp(MyApp());
