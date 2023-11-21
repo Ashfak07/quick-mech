7f4af9c5127a7aa06d0081298d92352d0d1925b8
Vyshnav k m <vyshnavkma06@gmail.com>
2023-11-21 17:01:07 +0530

    user db updated

--- lib/utils/database/user_db.dart
+++ lib/utils/database/user_db.dart
@@ -3,8 +3,8 @@ import 'package:quickmech/model/user_model.dart';
 List<UserModel> userData = [
   UserModel(
     image: 'assets/jpg/avatar.jpg',
-    name: 'Arun',
+    name: 'Anu',
     mobile: '9879564817',
-    email: 'Arunkumar@gmail.com',
+    email: 'Anukumar@gmail.com',
   )
 ];
