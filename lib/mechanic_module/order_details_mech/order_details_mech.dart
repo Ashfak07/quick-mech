e2ab9b22198e5e7b5a50763a3f1d5eed0ac103d9
Vyshnav k m <vyshnavkma06@gmail.com>
2023-11-27 22:30:46 +0530

    add order details mech folder and file

---
+++ lib/mechanic_module/order_details_mech/order_details_mech.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderDetailsMech extends StatefulWidget {
+  const OrderDetailsMech({super.key});
+
+  @override
+  State<OrderDetailsMech> createState() => _OrderDetailsMechState();
+}
+
+class _OrderDetailsMechState extends State<OrderDetailsMech> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
